93bf6877374e44ae0d98dfde394b10b19c925f09
Shape581 <severusrogue5008@gmail.com>
2025-01-17 19:47:59 +0100

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Life;
+using Life.DB;
+using Life.InventorySystem;
+using Life.Network;
+using Life.UI;
+using Mirror;
+using Unity.Profiling.LowLevel.Unsafe;
+using Utils;
+
+namespace Object581
+{
+    public class Main : Plugin
+    {
+        public Main(IGameAPI api) : base(api) { }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+            Object.Init();
+            Nova.server.OnPlayerReceiveItemEvent += new Action<Player, int, int, int>(Receive);
+            Nova.server.OnPlayerDropItemEvent += new Action<Player, int, int, int>(Drop);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"[Object581] - Intialisé !");
+            Console.ResetColor();
+        }
+
+        public async void Receive(Player player, int itemId, int slotId, int quantity)
+        {
+            if (await Object.Check(i => i.ItemId == itemId))
+            {
+                Utils.Inventory.RemoveItem(player, itemId, quantity);
+                var item = Utils.Item.GetItem(itemId);
+                Notify.Send(player, $"Vous n'avez pas le droit de posséder l'objet {Format.Color(item.itemName, Format.Colors.Yellow)}, celui ci vous a été retirer.");
+            }
+        }
+
+        public async void Drop(Player player, int itemId, int slotId, int quantity)
+        {
+            if (await Object.Check(i => i.ItemId == itemId))
+            {
+                UnityEngine.Object.FindObjectsOfType<DroppedItem>().ToList().ForEach(droppedItem =>
+                {
+                    if (droppedItem.itemId == itemId)
+                    {
+                        NetworkServer.Destroy(droppedItem.gameObject);
+                    }
+                });
+            }
+        }
+
+        public override async void OnPlayerSpawnCharacter(Player player, NetworkConnection conn, Characters character)
+        {
+            base.OnPlayerSpawnCharacter(player, conn, character);
+            foreach (var elements in Utils.Inventory.GetPlayerInventory(player))
+            {
+                int itemId = elements.Key;
+                int quantity = elements.Value;
+                if (await Object.Check(i => i.ItemId == itemId))
+                {
+                    Utils.Inventory.RemoveItem(player, itemId, quantity);
+                    var item = Utils.Item.GetItem(itemId);
+                    Notify.Send(player, $"Vous n'avez pas le droit de posséder l'objet {Format.Color(item.itemName, Format.Colors.Yellow)}, celui ci vous a été retirer.");
+                }
+            }
+        }
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,104 @@
+﻿using Life;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Life.UI;
+using Life.Network;
+using Utils;
+using System.Net.Mail;
+using System.Security.AccessControl;
+
+namespace Object581
+{
+    public class Menu : Plugin
+    {
+        public Menu(IGameAPI api) : base(api) { }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+            Command.Create("/object", "Menu Object581", player =>
+            {
+                OpenMenu(player);
+            });
+        }
+
+        public void OpenMenu(Player player)
+        {
+            var panel = new UIPanel(Format.Color("Object581"), UIPanel.PanelType.Text);
+            panel.AddButton(Format.Color("Ajouter", Format.Colors.Green), ui =>
+            {
+                AddItem(player);
+            });
+            panel.AddButton(Format.Color("Supprimer", Format.Colors.Red), ui =>
+            {
+                DeleteItem(player);   
+            });
+            panel.AddButton("Fermer", ui => player.ClosePanel(panel));
+            player.ShowPanelUI(panel);
+        }
+
+        public async void DeleteItem(Player player)
+        {
+            var panel = new UIPanel(Format.Color("Supprimer", Format.Colors.Red), UIPanel.PanelType.TabPrice);
+            panel.AddButton("Annuler", ui => OpenMenu(player));
+            panel.AddButton(Format.Color("Supprimer", Format.Colors.Red), ui => ui.SelectTab());
+            foreach (var elements in await Object.RequestAll())
+            {
+                var item = Item.GetItem(elements.ItemId);
+                panel.AddTabLine(Format.Color($"{item.itemName}"), "", Item.GetIconId(elements.ItemId), async ui =>
+                {
+                    await elements.Delete();
+                    Notify.Send(player, "Vous avez supprimer cette objet.", Notify.Type.Success);
+                    OpenMenu(player);
+                });
+            }
+            player.ShowPanelUI(panel);
+        }
+
+        public void AddItem(Player player)
+        {
+            var panel = new UIPanel(Format.Color("Ajout", Format.Colors.Green), UIPanel.PanelType.Input);
+            panel.SetText("<b>Ajouter un Objet.</b>");
+            panel.SetInputPlaceholder("Entrée l'id de l'item...");
+            panel.AddButton("Annuler", ui => OpenMenu(player));
+            panel.AddButton(Format.Color("Valider", Format.Colors.Green), async ui =>
+            {
+                if (int.TryParse(panel.inputText, out int value))
+                {
+                    var item = Item.GetItem(value);
+                    if (item != null)
+                    {
+                        if (!await Object.Check(obj => obj.ItemId == value))
+                        {
+                            Object.Write(value);
+                            Notify.Send(player, "Vous avez ajouter cette objet.", Notify.Type.Success);
+                            OpenMenu(player);
+                        }
+                        else
+                        {
+                            Notify.Send(player, "Cette objet est déjà enregistrer.", Notify.Type.Error);
+                            player.ClosePanel(panel);
+                            AddItem(player);
+                        }
+                    }
+                    else
+                    {
+                        Notify.Send(player, "Veuillez entrée l'id d'un item existant.", Notify.Type.Error);
+                        player.ClosePanel(panel);
+                        AddItem(player);
+                    }
+                }
+                else
+                {
+                    Notify.Send(player, "Veuillez entrée u l'id d'un item valide.", Notify.Type.Error);
+                    player.ClosePanel(panel);
+                    AddItem(player);
+                }
+            });
+            player.ShowPanelUI(panel);
+        }
+    }
+}
