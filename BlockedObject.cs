28072706869a934581f526a3c74a7ed26eecdcc4
Shape581 <severusrogue5008@gmail.com>
2025-08-03 16:33:16 +0200

    Add files via upload

---
+++ BlockedObject.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object581
+{
+    public class BlockedObject
+    {
+        [AutoIncrement][PrimaryKey] public int Id { get; set; }
+        public int ItemId { get; set; }
+        public bool BlockedForStaff { get; set; }
+    }
+}
